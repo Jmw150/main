2bf5892e73e0df5780a993e9b38891911999cb74
Jordan Winkler <jmw150@hotmail.com>
2019-05-16 15:07:44 -0400

    upload

--- main/language/java/hellojava.java
+++ main/language/java/hellojava.java
@@ -1,12 +1,5 @@
 //hello world in java
 
-/*#include "stdio.h"
-int main () { 
-    printf("hello!\n"); 
-    return 0;
-}
-*/
-
 public class hellojava 
 {
 	public static void main (String[] args)
@@ -14,3 +7,5 @@ public class hellojava
 		System.out.println("Hello, World!");
 	}
 }
+
+
